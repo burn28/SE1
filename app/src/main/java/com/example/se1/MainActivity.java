a6dd4ed40ecf9cbeb5b3e01d2f03db0ebaf6e462
Abu Bakar <abbakker00@gmail.com>
2021-05-03 15:50:19 +0800

    initial commit

---
+++ app/src/main/java/com/example/se1/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.se1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button loginBtn;
+    private Button signUpBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loginBtn = findViewById(R.id.loginBtn);
+        signUpBtn = findViewById(R.id.signUpBtn);
+        loginBtn.setOnClickListener(this);
+        signUpBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.loginBtn:
+                startActivity(new Intent(this, LoginActivity.class));
+                break;
+            case R.id.signUpBtn:
+                startActivity(new Intent(this, RegisterActivity.class));
+                break;
+        }
+
+    }
+}
